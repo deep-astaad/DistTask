8f89852c4e08d02e00afd11277950baf56bd7b30
Aman Deep Singh <singh.de@sms-datatech.co.jp>
2026-05-18 02:16:42 +0900

    feat: update config to use all envs

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,40 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    redis_url: str = "redis://localhost:6379/0"
+    postgres_host: str = "postgres"
+    postgres_port: int = 5432
+    postgres_db: str = "disttask"
+    postgres_user: str = "disttask"
+    postgres_password: str = "disttask"
 
-    class Config:
-        env_file = ".env"
+    redis_host: str = "redis"
+    redis_port: int = 6379
+
+    worker_concurrency: int = 4
+    visibility_timeout: int = 60
+
+    default_queue: str = "default"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",
+    )
+
+    @property
+    def redis_url(self):
+        return f"redis://{self.redis_host}:{self.redis_port}/0"
+
+    @property
+    def postgres_url(self):
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.postgres_user}:"
+            f"{self.postgres_password}@"
+            f"{self.postgres_host}:"
+            f"{self.postgres_port}/"
+            f"{self.postgres_db}"
+        )
 
 
 settings = Settings()
